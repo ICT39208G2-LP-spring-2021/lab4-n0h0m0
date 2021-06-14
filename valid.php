6261e2295de4836bb994644a9d93c16584ec6496
n0h0m0 <aleksandregasparyan@gmail.com>
2021-06-15 02:29:43 +0400

    მეოთხე ლაბორატორიული სამუშაო

--- valid.php
+++ valid.php
@@ -54,47 +54,6 @@ if(empty($emailErr) && empty($passwordErr)) {
     $Err = "შეავსეთ ყველა ველი.";
 }
 
-
-//    if($_SESSION['verificated'] != 'guide'){
-//        $_SESSION['logged'] = true;
-//    }else{
-//        $Err = "გადადით მეილზე მოსულ ბმულზე რათა გაააქტიუროთ თქვენი გვერდი.";
-//    }
-
-
-//    while ($row = mysqli_fetch_assoc($checkEm)) {
-//        $_SESSION['pass'] = $row['Pass'];
-//        $_SESSION['email'] = $row['Email'];
-//    };
-
-//    if($email != $_SESSION['email']){
-//        $emailErr = "<p class='text-danger'> User with this email isn't exist. </p>";
-//    }
-//    if($password != $_SESSION['pass']){
-//        $passwordErr = "<p class='text-danger'> Wrong password. </p>";
-//    }
-
-
-//    if (empty($emailErr) && empty($passwordErr)){
-//        echo "TEST";
-//        $sqlEm = "SELECT * FROM users where Email = '$email'";
-//        $checkEm = mysqli_query($conn, $sqlEm);
-//        while ($row = mysqli_fetch_assoc($checkEm)) {
-//            $_SESSION['userID'] = $row['Id'];
-//            $_SESSION['firstname'] = $row['FirstName'];
-//            $_SESSION['lastname'] = $row['LastName'];
-//            $_SESSION['verificated'] = $row['EmailVerificationToken'];
-//        };
-//        $_SESSION['pass'] = "";
-//        $_SESSION['email'] = "";
-//        if ($_SESSION['verificated'] !== 'guide') {
-//            $_SESSION['logged'] = true;
-//        } else {
-//            $Err = "გადადით მეილზე მოსულ ბმულზე რათა გაააქტიუროთ თქვენი გვერდი.";
-//        }
-//
-//    }
-    //header('Location: index.php');
 }
 
 ?>
